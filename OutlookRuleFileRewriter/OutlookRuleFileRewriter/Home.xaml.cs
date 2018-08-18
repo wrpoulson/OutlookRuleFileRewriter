9fb89b128a5689e277f77dd9a7b04d2eea91610b
William R Poulson <wrpoulson@gmail.com>
2018-08-17 20:47:50 -0400

    Started working on WPF app.

---
+++ OutlookRuleFileRewriter/OutlookRuleFileRewriter/Home.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OutlookRuleFileRewriter
+{
+    /// <summary>
+    /// Interaction logic for Menu.xaml
+    /// </summary>
+    public partial class Home : Page
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Stories_Click(object sender, RoutedEventArgs e)
+        {
+            AddStories addStories = new AddStories();
+            this.NavigationService.Navigate(addStories);
+        }
+
+
+        private void Remove_Stories_Click(object sender, RoutedEventArgs e)
+        {
+            RemoveStories removeStories = new RemoveStories();
+            this.NavigationService.Navigate(removeStories);
+        }
+    }
+}
---
+++ OutlookRuleFileRewriter/OutlookRuleFileRewriter/RemoveStories.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OutlookRuleFileRewriter
+{
+    /// <summary>
+    /// Interaction logic for RemoveStories.xaml
+    /// </summary>
+    public partial class RemoveStories : Page
+    {
+        public RemoveStories()
+        {
+            InitializeComponent();
+            PopulateStoriesListBox();
+        }
+
+        string[] stories = new string[] { "XP-7102", "XP-6345", "XP-3479"};
+
+        private void PopulateStoriesListBox()
+        {
+            stories.Select(s => storiesListBox.Items.Add(s));            
+        }
+
+        private void Remove_Stories_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
